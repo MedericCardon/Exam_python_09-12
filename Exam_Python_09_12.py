6bc1b4319fc8bc6dfe419a0122b34d710806f685
CardonMederic <medericcardon@gmail.com>
2020-12-09 14:56:37 +0100

    2

--- Exam_Python_09_12.py
+++ Exam_Python_09_12.py
@@ -6,10 +6,12 @@ print(Style.RESET_ALL)
 print('back to normal now')
 
 
+mot1=["c","i","t","r","o","n"]
 
 
+def mot_desordre (mot1):
+    for i in range (len(mot1)):
+        print(mot1)
 
-
-
-
+mot_desordre(mot1)
 input()
