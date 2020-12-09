5d0358ce5d61332934fd6a123c1ff2005f8b8963
CardonMederic <medericcardon@gmail.com>
2020-12-09 15:37:51 +0100

    6

--- Exam_Python_09_12.py
+++ Exam_Python_09_12.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 from colorama import Fore, Back, Style
 print(Fore.WHITE + 'some red text')
 print(Back.YELLOW + 'and with a green background')
@@ -6,21 +10,33 @@ print(Style.RESET_ALL)
 print('back to normal now')
 
 
-mot1=["c","i","t","r","o","n"] 
-proposition = (input("Choisir votre mot (6 lettres)"))
+mot1=["c","i","t","r","o","n"]
+mot2=["t","o","m","a","t","e"]
+mot3=["c","i","t","r","o","n"]
+mot4=["c","i","t","r","o","n"]
+mot5=["c","i","t","r","o","n"]
+mot6=["c","i","t","r","o","n"]
+mot7=["c","i","t","r","o","n"]
+mot8=["c","i","t","r","o","n"]
+mot9=["c","i","t","r","o","n"]
+mot10=["c","i","t","r","o","n"]
+
+
+proposition = random.(1,2,3,4,5,6,7,8,9,10)
+print(proposition)
 
 def compare_mot (mot1,proposition):
     for i in range (len(proposition)):
         if proposition[i] == mot1[i]:
             print(Back.RED + proposition[i], end="")
+            print("félicitation vous avez gagné !")
         if proposition[i] != mot1[i]:
             print(Back.YELLOW + proposition[i], end="")
       
     
-        
 
+    
 
-compare_mot(mot1,proposition)
 
 
 
