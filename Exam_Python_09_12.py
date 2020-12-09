8f232db904386ae54f68a9c394c187d7d3b7097f
CardonMederic <medericcardon@gmail.com>
2020-12-09 14:46:49 +0100

    1

--- Exam_Python_09_12.py
+++ Exam_Python_09_12.py
@@ -1,1 +1,15 @@
-fbf
+from colorama import Fore, Back, Style
+print(Fore.WHITE + 'some red text')
+print(Back.YELLOW + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+
+
+
+
+
+
+
+input()
